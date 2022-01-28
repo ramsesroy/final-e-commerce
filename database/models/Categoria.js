8d9e6902322696b54444fbccb863809dbed173df
ramsesroy <ramsesadorno@gmail.com>
2022-01-28 18:00:54 -0300

    Test

---
+++ database/models/Categoria.js
@@ -0,0 +1,17 @@
+module.exports = (sequelize, DataTypes) => {
+
+    let cols = {
+       
+    };
+
+    let config = {
+       
+    };
+
+    const Categoria = sequelize.define("Categoria", cols, config);
+
+  
+
+    return Categoria;
+
+}
